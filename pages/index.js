2e50a3836be1353cf7125132fd32c582949e73ab
ruiyigan <85063215+ruiyigan@users.noreply.github.com>
2022-07-09 19:07:48 +0800

    camera functionality

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,11 @@ import { useRouter } from 'next/router'
 import { useState, useEffect } from 'react'
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from '../firebase-services/config'
-
+import Camera, { IMAGE_TYPES } from 'react-html5-camera-photo';
+import 'react-html5-camera-photo/build/css/index.css';
 
 function AuthStateListener(isLoading, setIsLoading) {
   const router = useRouter()
-
   useEffect(() => {
     // Listener
     onAuthStateChanged(auth, (user) => {
@@ -28,11 +28,39 @@ function AuthStateListener(isLoading, setIsLoading) {
 }
 
 export default function Home() {
-
   const [isLoading, setIsLoading] = useState(true)
+  const [openCamera, setOpenCamera] = useState(false)
+  const [dataUri, setDataUri] = useState('');
+  const isFullscreen = false;
+  const handleTakePhoto = (data) => {
+    const fileResult = dataURItoFileData(data)
+    console.log(fileResult)
+    setDataUri(data);
+    setOpenCamera(false)
+  }
+
+  const dataURItoFileData = (dataURI) => {
+    // convert base64 to raw binary data held in a string
+    // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+    var byteString = atob(dataURI.split(',')[1]);
+
+    // separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    // write the bytes of the string to an ArrayBuffer
+    var ab = new ArrayBuffer(byteString.length);
+    var ia = new Uint8Array(ab);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+    //New Code
+    const blob =  new Blob([ab], {type: mimeString});
+    const fd = new FormData(document.forms[0]);
+    fd.append("file", blob);
+    return fd.get("file");
+  }
 
   AuthStateListener(isLoading, setIsLoading)
-
   if (isLoading) {
     return (
       <div>
@@ -43,11 +71,19 @@ export default function Home() {
 
   return (
     <div>
+      {openCamera
+        &&
+        <Camera
+          onTakePhotoAnimationDone={handleTakePhoto}
+          isFullscreen={isFullscreen}
+          imageType={IMAGE_TYPES.JPG}
+        />}
+
       <p>Welcome, you are logged in</p>
       <div className='flex py-10'>
-        <CameraIcon className='h-15 w-14 ml-auto mr-auto' onClick={() => console.log("clicked")}/>
+        <CameraIcon className='h-15 w-14 ml-auto mr-auto' onClick={() => setOpenCamera(true)} />
       </div>
-      <LogoutButton/>
+      <LogoutButton />
     </div>
   )
 }
